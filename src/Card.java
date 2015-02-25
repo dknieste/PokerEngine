56593eb30a945fb549e7c787b46ac7f6823e987e
dknieste <dknieste@udel.edu>
2015-02-25 15:57:38 -0500

    commit

---
+++ src/Card.java
@@ -0,0 +1,9 @@
+// Private attributes, public methods
+
+public class Card {
+
+	private int rank;
+	private int suit;
+	
+	
+}
---
+++ src/Deck.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Deck {
+
+	private ArrayList<Card> deck = new ArrayList<Card>();
+	private int NumberOfCards;
+	private final int DeckSize = 52;
+	
+}
